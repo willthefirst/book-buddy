c42746ac8dd0e12635bb2c4cf0bf3543c8348b32
Will Moritz <will@crowdfavorite.com>
2017-02-06 11:55:02 -0500

    cleaned up /book/info route

--- src/routes/Book/components/Book.js
+++ src/routes/Book/components/Book.js
@@ -1,72 +1,20 @@
 import React from 'react'
-import { Button, Col, Row, Tabs, Tab, Form, FormGroup, ControlLabel, FormControl, Checkbox, Radio } from 'react-bootstrap';
+import { Button, Col, Row, Nav, NavItem } from 'react-bootstrap';
 
 export const Book = (props) => (
   <div>
-    <div>{props.children}</div>
     <h1>{props.book.title}</h1>
     <Row>
       <Col sm={3} >
         <img src="http://placehold.it/200x300" className="img-responsive" style={{ marginBottom: '24px', width: '100%' }} />
       </Col>
       <Col sm={9}>
-        <Tabs defaultActiveKey={1} bsStyle="pills" id="book-info">
-          <Tab eventKey={1} title="Progress">Progress content</Tab>
-          <Tab eventKey={2} title="Notes">Notes content</Tab>
-          <Tab eventKey={3} title="Info">
-
-            <Form className="core-layout__viewport" horizontal>
-              <FormGroup controlId="formHorizontalTitle">
-                <Col componentClass={ControlLabel} sm={2}>
-                  Title
-                </Col>
-                <Col sm={10}>
-                  <FormControl type="text" placeholder="Title" />
-                </Col>
-              </FormGroup>
-
-              <FormGroup controlId="formHorizontalAuthor">
-                <Col componentClass={ControlLabel} sm={2}>
-                  Author
-                </Col>
-                <Col sm={10}>
-                  <FormControl type="text" placeholder="Author" />
-                </Col>
-              </FormGroup>
-
-              <FormGroup controlId="formHorizontalStatus">
-                <Col componentClass={ControlLabel} sm={2}>
-                  Status
-                </Col>
-                <Col sm={10}>
-                  <FormControl componentClass="select" placeholder="Status">
-                    <option value="current">Current</option>
-                    <option value="queue">Queue</option>
-                    <option value="finished">Finished</option>
-                  </FormControl>
-                </Col>
-              </FormGroup>
-
-              <FormGroup controlId="formHorizontalTotalPages">
-                <Col componentClass={ControlLabel} sm={2}>
-                  Total Pages
-                </Col>
-                <Col sm={10}>
-                  <FormControl type="number" placeholder="0" />
-                </Col>
-              </FormGroup>
-
-              <FormGroup>
-                <Col smOffset={2} sm={10}>
-                  <Button type="submit">
-                    Save
-                  </Button>
-                </Col>
-              </FormGroup>
-
-            </Form>
-          </Tab>
-        </Tabs>
+        <Nav bsStyle="tabs" id="book-info">
+          <NavItem eventKey={1} title="Progress" href="/book/progress">Progress</NavItem>
+          <NavItem eventKey={2} title="Notes" href="/book/notes" >Notes</NavItem>
+          <NavItem eventKey={3} title="Info" href="/book/info">Info</NavItem>
+        </Nav>
+        <div>{props.children}</div>
       </Col>
     </Row>
   </div>
--- src/routes/Book/routes/Info/components/Info.js
+++ src/routes/Book/routes/Info/components/Info.js
@@ -1,8 +1,58 @@
 import React from 'react'
+import { Col, Button, Form, FormGroup, ControlLabel, FormControl, Checkbox, Radio } from 'react-bootstrap'
 
 export const Info = (props) => (
   <div>
-    <h1>INFO</h1>
+    <Form className="core-layout__viewport" horizontal>
+      <FormGroup controlId="formHorizontalTitle">
+        <Col componentClass={ControlLabel} sm={2}>
+          Title
+        </Col>
+        <Col sm={10}>
+          <FormControl type="text" placeholder="Title" />
+        </Col>
+      </FormGroup>
+
+      <FormGroup controlId="formHorizontalAuthor">
+        <Col componentClass={ControlLabel} sm={2}>
+          Author
+        </Col>
+        <Col sm={10}>
+          <FormControl type="text" placeholder="Author" />
+        </Col>
+      </FormGroup>
+
+      <FormGroup controlId="formHorizontalStatus">
+        <Col componentClass={ControlLabel} sm={2}>
+          Status
+        </Col>
+        <Col sm={10}>
+          <FormControl componentClass="select" placeholder="Status">
+            <option value="current">Current</option>
+            <option value="queue">Queue</option>
+            <option value="finished">Finished</option>
+          </FormControl>
+        </Col>
+      </FormGroup>
+
+      <FormGroup controlId="formHorizontalTotalPages">
+        <Col componentClass={ControlLabel} sm={2}>
+          Total Pages
+        </Col>
+        <Col sm={10}>
+          <FormControl type="number" placeholder="0" />
+        </Col>
+      </FormGroup>
+
+      <FormGroup>
+        <Col smOffset={2} sm={10}>
+          <Button type="submit">
+            Save
+          </Button>
+        </Col>
+      </FormGroup>
+
+    </Form>
   </div>
 )
 
