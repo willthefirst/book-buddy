60c14e08d5c006df808bf8af7afbc763f117a4e1
Will Moritz <will@crowdfavorite.com>
2017-02-10 12:44:17 -0500

    added a basic text editor

--- src/routes/Book/routes/Notes/components/Notes.js
+++ src/routes/Book/routes/Notes/components/Notes.js
@@ -1,11 +1,29 @@
-import React from 'react'
-import { FormGroup, ControlLabel, FormControl } from 'react-bootstrap'
+import React, { Component} from 'react'
+import { Editor } from 'react-draft-wysiwyg'
+// #todo: customize text editor: https://jpuri.github.io/react-draft-wysiwyg/#/docs
+// also https://github.com/jpuri/react-draft-wysiwyg/blob/master/docs/src/components/Demo/index.js
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css'
 
-export const Notes = (props) => (
-  <FormGroup controlId="formControlsTextarea">
-    <FormControl componentClass="textarea" placeholder="Write your notes here..." />
-  </FormGroup>
-)
+export class Notes extends Component {
+
+  render() {
+    const toolbarOptions = {
+      options: ['inline', 'blockType', 'fontSize', 'fontFamily', 'remove', 'history'],
+      inline: {
+        options: ['bold', 'italic', 'underline', 'strikethrough'],
+      },
+    }
+
+    return (
+      <Editor
+        wrapperClassName="react-draft__wrapper"
+        editorClassName="react-draft__editor"
+        toolbarClassName="react-draft__toolbar"
+        toolbar={toolbarOptions}
+        />
+    )
+  }
+}
 
 Notes.propTypes = {
 
