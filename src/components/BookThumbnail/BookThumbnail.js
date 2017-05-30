3124abf9e95042130675624efc12d143f734a61d
Will Moritz <will@crowdfavorite.com>
2017-05-30 16:44:44 -0400

    better footer

--- src/components/BookThumbnail/BookThumbnail.js
+++ src/components/BookThumbnail/BookThumbnail.js
@@ -34,7 +34,7 @@ const BookThumbnail = (props) => {
   }
 
   return (
-    <Col xs={3} sm={2} md={1}>
+    <Col xs={4} sm={2}>
       <div className='book-thumb__container'>
         { thumbnail }
         { info }
